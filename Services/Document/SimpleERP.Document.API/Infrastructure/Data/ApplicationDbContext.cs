e21a0a52c4d2db9f6733f82c791901661270dec7
shahab.sharafi <shahab.sharafi@gmail.com>
2020-03-12 13:25:10 +0330

    migration

--- Services/Document/SimpleERP.Document.API/Infrastructure/Data/ApplicationDbContext.cs
+++ Services/Document/SimpleERP.Document.API/Infrastructure/Data/ApplicationDbContext.cs
@@ -21,17 +21,6 @@ namespace SimpleERP.Document.API.Infrastructure.Data
         public DbSet<Domain> Domain { get; }
         public DbSet<Issuer> Issuer { get; }
         public DbSet<Type> Type { get; }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            var schema = "DocumentManagement";
-            builder.Entity<DocumentInfo>().ToTable("DocumentInfos", schema);
-            builder.Entity<Domain>().ToTable("DocumentInfos", schema);
-            builder.Entity<Issuer>().ToTable("DocumentInfos", schema);
-            builder.Entity<Type>().ToTable("DocumentInfos", schema);
-
-        }
     }
 }
 
