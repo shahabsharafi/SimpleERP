b90addce03b2e2f7679801694860921e90c7d4e0
nouser <nopassword>
2020-08-18 11:10:13 -0700

    Prepare Production

--- UI/SimpleERP.UI.Web/Startup.cs
+++ UI/SimpleERP.UI.Web/Startup.cs
@@ -43,7 +43,7 @@ namespace SimpleERP.UI.Web
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             app.UseHttpsRedirection();
